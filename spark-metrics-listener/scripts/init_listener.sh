a358d1445daf82a85c67850b47e022eea0214089
vivekravichandiran <vivek.ravichandiran@databricks.com>
2026-03-03 12:19:07 +0530

    success

--- spark-metrics-listener/scripts/init_listener.sh
+++ spark-metrics-listener/scripts/init_listener.sh
@@ -23,7 +23,7 @@ mkdir -p ${JAR_DEST}
 
 # Copy JAR from DBFS to local filesystem
 echo "[init_listener] Copying JAR from ${JAR_SOURCE} to ${JAR_DEST}..."
-cp /dbfs/databricks/jars/spark-metrics-listener-1.0.0.jar ${JAR_DEST}
+cp ${JAR_SOURCE} ${JAR_DEST}
 
 echo "[init_listener] JAR copied successfully."
 echo "[init_listener] CustomMetricsListener initialization complete."
